381e11ccc0ff868b8cd3bb3ec79a3dbc67c43f59
Odysseas Lamtzidis <odyslam@gmail.com>
2022-02-07 21:53:36 +0200

    feat: basic auth flow with siwe

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
+use ethers::core::utils::to_checksum;
+use ethers::types::{Address, Signature, H160};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
+use worker::Response;
 use worker::*;
-
 mod utils;
+use hex::FromHex;
+use rand::Rng;
+use sha2::{Digest, Sha256};
+use siwe::Message;
+use std::str::FromStr;
 
 fn log_request(req: &Request) {
     console_log!(
@@ -13,43 +21,137 @@ fn log_request(req: &Request) {
     );
 }
 
-#[event(fetch)]
-pub async fn main(req: Request, env: Env) -> Result<Response> {
+#[derive(Deserialize, Serialize)]
+struct AuthRequest {
+    message: String,
+    signature: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct Authorization {
+    resources: Vec<String>,
+    issued_at: String,
+    expiration_time: Option<String>,
+    not_before: Option<String>,
+    address: H160,
+}
+
+impl Authorization {
+    async fn from_req(env: &Env, req: &Request) -> Result<Option<Authorization>> {
+        let headers = req.headers();
+        let bearer = headers.get("BEARER")?;
+        let cookie = headers.get("AUTH-SIWE")?;
+        let token = match bearer.or(cookie) {
+            Some(token) => token,
+            None => return Err(worker::Error::from("no authorization header found")),
+        };
+        let store = env.kv("AUTHENTICATION")?;
+        store
+            .get(&token)
+            .json::<Authorization>()
+            .await
+            .map_err(|error| worker::Error::from(error))
+    }
+    async fn is_authorized(env: &Env, req: &Request) -> Result<bool> {
+        match Authorization::from_req(env, req).await? {
+            Some(auth) => Ok(auth.resources.contains(&req.url()?.path().to_string())),
+            None => Err(worker::Error::from("could not find auth object")),
+        }
+    }
+}
+
+#[event(fetch, respond_with_errors)]
+pub async fn main(req: Request, env: Env, worker_ctx: Context) -> Result<Response> {
     log_request(&req);
-
-    // Optionally, get more helpful error messages written to the console in the case of a panic.
     utils::set_panic_hook();
-
-    // Optionally, use the Router to handle matching endpoints, use ":name" placeholders, or "*name"
-    // catch-alls to match on specific patterns. Alternatively, use `Router::with_data(D)` to
-    // provide arbitrary data that will be accessible in each route via the `ctx.data()` method.
+    let authorized: bool = Authorization::is_authorized(&env, &req).await?;
     let router = Router::new();
-
-    // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
-    // functionality and a `RouteContext` which you can use to  and get route parameters and
-    // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
     router
-        .get("/", |_, _| Response::ok("Hello from Workers!"))
-        .post_async("/form/:field", |mut req, ctx| async move {
-            if let Some(name) = ctx.param("field") {
-                let form = req.form_data().await?;
-                match form.get(name) {
-                    Some(FormEntry::Field(value)) => {
-                        return Response::from_json(&json!({ name: value }))
-                    }
-                    Some(FormEntry::File(_)) => {
-                        return Response::error("`field` param in form shouldn't be a File", 422);
-                    }
-                    None => return Response::error("Bad Request", 400),
-                }
-            }
-
-            Response::error("Bad Request", 400)
-        })
-        .get("/worker-version", |_, ctx| {
-            let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
+        .get("/api/v0/info", |req, _ctx| {
+            let version = "0.1";
+            console_log!("{}", req.url()?.path());
             Response::ok(version)
         })
-        .run(req, env)
+        .post_async("/authorize", |mut req, ctx| async move {
+            let body = req
+                .json::<AuthRequest>()
+                .await
+                .map_err(|error| worker::Error::from(format!("body parsing: {:?}", error)))?;
+            let signature = <[u8; 65]>::from_hex(body.signature.trim_start_matches("0x"))
+                .map_err(|error| worker::Error::from(format!("signature parsing: {:?}", error)))?;
+            let message: Message = body.message.parse().map_err(|error| {
+                worker::Error::from(format!("siwe message parsing: {:?}", error))
+            })?;
+            match message.verify(signature) {
+                Ok(_) => {
+                    let authentication = ctx.kv("AUTHENTICATION")?;
+                    let mut rng = rand::thread_rng();
+                    let mut hasher = Sha256::new();
+                    let auth = Authorization {
+                        resources: message
+                            .resources
+                            .iter()
+                            .map(|x| x.as_str().to_owned())
+                            .collect::<Vec<String>>(),
+                        issued_at: format!("{}", message.issued_at),
+                        expiration_time: message.expiration_time.clone().map(|x| format!("{}", x)),
+                        not_before: message.not_before.map(|x| format!("{}", x)),
+                        address: H160(message.address),
+                    };
+                    let auth_string: String = serde_json::to_string(&auth).unwrap();
+                    hasher.update(auth_string.as_bytes());
+                    // add salt to the auth token
+                    hasher.update(rng.gen::<[u8; 32]>());
+                    let hash = format!("{:X}", hasher.finalize());
+                    // store the auth object of a user with the auth token as key
+                    // the auth object KV will expire when then SIWE message expires as well.
+                    // That way, we don't have stale auth objects in our KV store
+                    authentication
+                        .put(&hash, &auth_string)?
+                        .expiration(
+                            message
+                                .expiration_time
+                                .unwrap()
+                                .as_ref()
+                                .timestamp()
+                                .unsigned_abs(),
+                        )
+                        .execute()
+                        .await?;
+                    let mut headers = Headers::new();
+                    console_log!(
+                        r#"
+########
+New session:
+user: {}
+key: {}
+value: {}
+########
+"#,
+                        to_checksum(&H160(message.address), Some(0)),
+                        &hash,
+                        &auth_string
+                    );
+                    headers.set(
+                        "Set-cookie",
+                        &format!(
+                            "SIWE-AUTH={}; Secure; HttpOnly; SameSite=Lax; Expires={}",
+                            &hash,
+                            Date::now().to_string()
+                        ),
+                    )?;
+                    let res =
+                        Response::redirect(worker::Url::from_str("http:/localhost/").unwrap())
+                            .unwrap()
+                            .with_headers(headers);
+                    return Ok(res);
+                }
+                Err(error) => {
+                    return Response::from_json(&json!(
+                        {"verified": false, "error" : format!("{:?}", error) }))
+                }
+            }
+        })
+        .run(req, worker_env)
         .await
 }
