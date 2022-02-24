992c177badc11f2f200f78e512525235afdfdfd1
Odysseas Lamtzidis <odyslam@gmail.com>
2022-02-24 17:20:52 +0200

    feat: add and update workstreams

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use ethers::types::Address;
 use std::collections::HashMap;
 use std::str::FromStr;
 use users::User;
+use worker::wasm_bindgen::UnwrapThrowExt;
 use worker::*;
 use workstreams::Workstream;
 mod auth;
@@ -43,47 +44,80 @@ pub async fn main(req: Request, env: Env, worker_ctx: Context) -> Result<Respons
             console_log!("{}", req.url()?.path());
             Response::ok(version)
         })
-        .get_async(
-            "/users/:user_address/workstreams/*workstream_id",
-            |_req, ctx| async move {
+        .on_async("/users/:user/workstreams", |mut req, ctx| async move {
+            let addr_string = ctx.param("user").unwrap();
+            return match req.method() {
+                Method::Post => {
+                    if !is_authorized(&req, &ctx.env, &ctx).await? {
+                        return Response::error("Unauthorized", 401);
+                    }
+                    let mut workstream = req.json::<Workstream>().await?;
+                    Workstream::populate(&mut workstream, addr_string, &ctx.env).await?;
+                    let store = ctx.kv("USERS")?;
+                    let mut user =
+                        if let Some(user) = store.get(&addr_string).json::<User>().await? {
+                            user
+                        } else {
+                            User {
+                                workstreams: HashMap::new(),
+                            }
+                        };
+                    user.workstreams.insert(workstream.id.clone(), workstream);
+                    store.put(&addr_string, user)?;
+                    Response::ok("workstream created")
+                }
+                Method::Get => {
+                    return match ctx.kv("USERS")?.get(addr_string).json::<User>().await? {
+                        Some(user) => Response::from_json(&user.workstreams),
+                        None => Response::error("User not found", 404),
+                    };
+                }
+                _ => Response::error("HTTP Method Not Allowed", 405),
+            };
+        })
+        .on_async(
+            "/users/:user/workstreams/:workstream_id",
+            |mut req, ctx| async move {
                 let workstream_id = ctx
                     .param("workstream_id")
                     .unwrap()
                     .strip_prefix('/')
                     .unwrap();
-                let address = ctx.param("user_address").unwrap();
-                console_log!("user: {}, requested workstream: {}", address, workstream_id);
-                return match ctx.kv("USERS")?.get(address).json::<User>().await? {
-                    Some(user) => {
-                        if workstream_id.is_empty() {
-                            Response::from_json(&user.workstreams)
-                        } else {
-                            Response::from_json(&user.workstreams.get(workstream_id))
+                let addr_string = ctx.param("user").unwrap();
+                let mut workstream: Workstream = req.json::<Workstream>().await?;
+                console_log!(
+                    "user: {}, performed action {:?} to workstream id: {} with workstream \n:{:?}",
+                    addr_string,
+                    req.method(),
+                    workstream_id,
+                    workstream
+                );
+                return match req.method() {
+                    Method::Put => {
+                        if !is_authorized(&req, &ctx.env, &ctx).await? {
+                            return Response::error("Unauthorized", 401);
                         }
+                        let store = ctx.kv("USERS")?;
+                        if let Some(mut user) = store.get(&addr_string).json::<User>().await? {
+                            let mut workstream_old =
+                                user.workstreams.get_mut(&workstream.id).unwrap_throw();
+                            let workstream_new = req.json::<Workstream>().await?;
+                            Workstream::update(&mut workstream_old, workstream_new)?;
+                            store.put(&addr_string, user)?.execute().await?;
+                            return Response::ok("workstream updated");
+                        }
+                        Response::ok("workstream updated")
                     }
-                    None => Response::error("User not found", 404),
+                    Method::Get => {
+                        return match ctx.kv("USERS")?.get(addr_string).json::<User>().await? {
+                            Some(user) => Response::from_json(&user.workstreams.get(workstream_id)),
+                            None => Response::error("User not found", 404),
+                        };
+                    }
+                    _ => Response::error("HTTP Method Not Allowed", 405),
                 };
             },
         )
-        .post_async("/users/:user/workstreams", |mut req, ctx| async move {
-            if !is_authorized(&req, &ctx.env, &ctx).await? {
-                return Response::error("Unauthorized", 401);
-            }
-            let addr_string = ctx.param("user").unwrap();
-            let mut workstream: Workstream = req.json::<Workstream>().await?;
-            Workstream::populate(&mut workstream, addr_string, &ctx.env).await?;
-            let store = ctx.kv("USERS")?;
-            let mut user = if let Some(user) = store.get(&addr_string).json::<User>().await? {
-                user
-            } else {
-                User {
-                    workstreams: HashMap::new(),
-                }
-            };
-            user.workstreams.insert(workstream.id.clone(), workstream);
-            store.put(&addr_string, user)?;
-            Response::ok("workstream created")
-        })
         .post_async("/authorize/", |req, ctx| async move {
             let auth_req: AuthRequest = AuthRequest::from_req(req).await?;
             let token: String = Authorization::create(&ctx.env, auth_req).await?;
--- src/workstreams.rs
+++ src/workstreams.rs
@@ -1,26 +1,28 @@
 use ethers::types::Address;
+use ethers::utils::to_checksum;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use worker::{wasm_bindgen::UnwrapThrowExt, Date, DateInit, Env, Error};
-#[derive(Deserialize, Serialize)]
+
+#[derive(Debug, Deserialize, Serialize, Eq, PartialEq)]
 pub enum WorkstreamType {
     Role,
     Grant,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Eq, PartialEq)]
 pub enum PaymentCurrency {
     DAI,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Eq, PartialEq)]
 pub enum WorkstreamState {
     Funded,
     Open,
     Finished,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Eq, PartialEq)]
 pub struct Application {
     id: String,
     description: String,
@@ -33,21 +35,21 @@ pub struct Application {
     state: ApplicationState,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Eq, PartialEq)]
 pub struct Receiver {
     address: Address,
     payment_rate: u32,
     payment_currency: PaymentCurrency,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub enum ApplicationState {
     Accepted,
     Rejected,
     Pending,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub struct Workstream {
     pub id: String,
     wtype: WorkstreamType,
@@ -55,14 +57,64 @@ pub struct Workstream {
     created_at: String,
     starting_at: Option<String>,
     ending_at: Option<String>,
-    receivers: Vec<Receiver>,
     description: String,
-    drips_hub: String,
+    drips_config: DripsConfig,
     state: WorkstreamState,
     applications: Option<Vec<Application>>,
 }
 
+#[derive(Debug, Deserialize, Serialize, PartialEq, Eq)]
+pub struct DripsConfig {
+    receivers: Vec<Receiver>,
+    drips_acct: u32,
+    drips_hub: Address,
+}
+
 impl Workstream {
+    pub fn update(
+        old_workstream: &mut Workstream,
+        new_workstream: Workstream,
+    ) -> Result<(), worker::Error> {
+        if old_workstream.drips_config != new_workstream.drips_config {
+            if !Workstream::check_drips_config(
+                &old_workstream.drips_config,
+                &new_workstream.drips_config,
+            )? {
+                return Err(worker::Error::from("wrong drips configuration"));
+            }
+            old_workstream.drips_config = new_workstream.drips_config;
+        }
+        if !Workstream::check_dates(
+            &new_workstream.created_at,
+            &new_workstream.starting_at,
+            &new_workstream.ending_at,
+        )? {
+            return Err(worker::Error::from("wrong date format"));
+        }
+        // Update time
+        old_workstream.created_at = new_workstream.created_at;
+        old_workstream.starting_at = new_workstream.starting_at;
+        old_workstream.ending_at = new_workstream.ending_at;
+        // update metadata
+        old_workstream.description = new_workstream.description;
+        old_workstream.wtype = new_workstream.wtype;
+        Ok(())
+    }
+    // check if passed receiver configuration actually exists on-chain
+    fn check_drips_config(
+        old_config: &DripsConfig,
+        new_config: &DripsConfig,
+    ) -> Result<bool, worker::Error> {
+        Ok(true)
+    }
+    // check if dates make sense (e.g created_at is before or at the same time with starting_at)
+    fn check_dates(
+        created_at: &String,
+        starting_at: &Option<String>,
+        ending_at: &Option<String>,
+    ) -> Result<bool, worker::Error> {
+        Ok(true)
+    }
     pub async fn populate(
         workstream: &mut Workstream,
         user: &str,
@@ -72,12 +124,14 @@ impl Workstream {
         workstream.creator = user.to_owned();
         workstream.state = WorkstreamState::Open;
         workstream.created_at = Date::now().to_string();
-        workstream.drips_hub = env
-            .kv("DRIPSHUBS")?
-            .get(&workstream.drips_hub)
-            .text()
-            .await?
-            .unwrap_throw();
+        workstream.drips_config.drips_hub = Address::from_slice(
+            env.kv("DRIPSHUBS")?
+                .get(&to_checksum(&workstream.drips_config.drips_hub, Some(1)))
+                .text()
+                .await?
+                .unwrap_throw()
+                .as_bytes(),
+        );
         if let Some(start) = &workstream.starting_at {
             if Date::now().as_millis() > Date::from(DateInit::String(start.to_string())).as_millis()
             {
@@ -92,4 +146,5 @@ impl Workstream {
         workstream.applications = None;
         Ok(())
     }
+    pub async fn put(old_workstream: &mut Workstream, new_workstream: &Workstream) {}
 }
