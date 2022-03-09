f9a10fcc651f2f36e62860b2f0044c9b513a1f36
Odysseas Lamtzidis <odyslam@gmail.com>
2022-03-09 09:52:22 +0200

    feat: docs generating script

---
+++ gen-docs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -rf ./target/doc
+cargo doc --document-private-items --no-deps --open --release
+rm -rf ./docs
+echo "<meta http-equiv=\"refresh\" content=\"0; url=workstreams_api\">" > target/doc/index.html
+cp -r target/doc ./docs
